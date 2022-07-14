3f8995c7470f2979aec613b5d948cf9c19a01c0d
Augusto <augustooliveixa@gmail.com>
2022-07-14 18:43:24 -0300

    Adicionando framewordk AlertifyJS

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'alertifyjs/build/css/alertify.css';
+import alertify from 'alertifyjs';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
